768b5b2c6a895bde0a547b09c13b66b33a69b19e
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-05-22 13:39:15 +0500

    Изменил приветствие

--- telegram_bot/management/commands/bot.py
+++ telegram_bot/management/commands/bot.py
@@ -1,16 +1,28 @@
 from django.core.management.base import BaseCommand
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from MeetUpPlanner import settings
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
-    CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я ваш бот.')
+    full_name = update.message.from_user.full_name
 
+    initial_message_text = (
+        f"Привет, {full_name}. Я PyMeetBot — твой помощник на Мероприятия! 🎉\n"
+        "Я помогу тебе задавать вопросы докладчикам, следить за программой и "
+        "получать уведомления."
+    )
+    event_details_text = (
+        "Мероприятие пройдет ..."
+    )
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    registration_keyboard = [
+        [InlineKeyboardButton("Зарегистрироваться", callback_data='register')]
+    ]
+    reply_markup = InlineKeyboardMarkup(registration_keyboard)
+
+    update.message.reply_text(initial_message_text)
+    update.message.reply_text(event_details_text, reply_markup=reply_markup)
 
 
 class Command(BaseCommand):
@@ -21,9 +33,6 @@ class Command(BaseCommand):
         dispatcher = updater.dispatcher
 
         dispatcher.add_handler(CommandHandler('start', start))
-        dispatcher.add_handler(MessageHandler(
-            Filters.text & ~Filters.command, echo)
-        )
 
         updater.start_polling()
         updater.idle()

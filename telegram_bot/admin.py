a815707fbb2605e2ab1274bb4522b8ab32fa4f4e
juneshone <shamrova.julia@mail.ru>
2024-05-22 17:31:39 +0300

    add model registration

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from telegram_bot.models import User, Event, Question
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('telegram_id', 'phonenumber', 'username',)
+    list_display = ('role', 'telegram_id', 'username', 'phonenumber',)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ('title', 'speaker', 'start_time',)
+    raw_id_fields = ('speaker',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ('description',)
+    list_display = ('description', 'listener', 'speaker', 'status',)
